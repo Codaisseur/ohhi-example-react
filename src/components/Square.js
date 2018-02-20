2d76ecc47defb6127e8c37e365170ae2f8cb803b
Wouter de Vos <wouter@codaisseur.com>
2018-02-20 15:43:45 +0100

    Locked squares

--- src/components/Square.js
+++ src/components/Square.js
@@ -9,24 +9,31 @@ export class Square extends PureComponent {
     value: PropTypes.number.isRequired,
     x: PropTypes.number.isRequired,
     y: PropTypes.number.isRequired,
-    makeMove: PropTypes.func.isRequired
+    makeMove: PropTypes.func.isRequired,
+    locked: PropTypes.bool
   }
 
   handleClick = () => {
-    const { x, y, makeMove } = this.props
+    const { x, y, makeMove, locked } = this.props
+    if (locked) return
+
     makeMove(y, x)
   }
 
   render() {
-    const { value } = this.props
+    const { value, locked } = this.props
 
     return (
       <div
-        className={`Square fill-${value || 0}`}
+        className={`Square fill-${value || 0}${locked ? ' locked': ''}`}
         onClick={this.handleClick}
       />
     )
   }
 }
 
-export default connect(null, { makeMove: move })(Square)
+const mapStateToProps = ({ locked }, { x, y }) => ({
+  locked: locked.filter(l => l[0] === y && l[1] === x).length > 0
+})
+
+export default connect(mapStateToProps, { makeMove: move })(Square)
